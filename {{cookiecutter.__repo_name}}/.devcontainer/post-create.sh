2f6cd54c59a6dcc31cdd73165d6bf1f0ca428ec2
tjarbo <16938041+tjarbo@users.noreply.github.com>
2026-03-25 16:51:13 +0100

    feat: add devcontainer

---
+++ {{cookiecutter.__repo_name}}/.devcontainer/post-create.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Plugin specific post-create.sh script.
+
+echo "Nothing to do";
+exit 0;
---
+++ {{cookiecutter.__repo_name}}/.devcontainer/post-start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Plugin specific post-start.sh script.
+
+echo "Nothing to do";
+exit 0;
