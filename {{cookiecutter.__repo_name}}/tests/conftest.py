be3a763886bdb7eba2b64cc23739e4a378d60d04
Tobias Kunze <r@rixx.de>
2026-09-01 16:57:04 +0200

    Standardise tests

--- {{cookiecutter.__repo_name}}/tests/conftest.py
+++ {{cookiecutter.__repo_name}}/tests/conftest.py
@@ -8,6 +8,7 @@ from django_scopes import scopes_disabled
 from pretalx.event.domain.event import initialise_event
 from pretalx.event.domain.plugins import enable_plugin
 from pretalx.event.models import Event, Organiser, Team
+from pretalx.person.enums import EmailVerificationState
 from pretalx.person.models import User
 
 
@@ -61,6 +62,7 @@ def orga_user(event):
             password="orgapassw0rd",
             email="orgauser@orga.org",
             name="Orga User",
+            email_verification_state=EmailVerificationState.VERIFIED,
         )
         team = event.organiser.teams.filter(
             can_change_organiser_settings=True, is_reviewer=False
@@ -77,6 +79,7 @@ def review_user(event):
             password="reviewpassw0rd",
             email="reviewuser@orga.org",
             name="Review User",
+            email_verification_state=EmailVerificationState.VERIFIED,
         )
         team = event.organiser.teams.filter(
             can_change_organiser_settings=False, is_reviewer=True
